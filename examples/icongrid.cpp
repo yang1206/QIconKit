b987fb71686f73cca8779a24a99d41e3efeb883e
yang1206 <yang1206@outlook.com.tr>
2025-03-04 22:05:41 +0800

    feat: 图标浏览器新增翻页，优化性能

--- examples/icongrid.cpp
+++ examples/icongrid.cpp
@@ -55,14 +55,12 @@ void IconGrid::setupUI() {
     m_searchTimer = new QTimer(this);
     m_searchTimer->setSingleShot(true);
     m_searchTimer->setInterval(300); // 300ms 延迟
-    
+
     connect(m_searchBox, &QLineEdit::textChanged, this, [this](const QString& text) {
         m_searchTimer->stop();
         m_searchTimer->start();
     });
-    connect(m_searchTimer, &QTimer::timeout, this, [this]() {
-        filterIcons(m_searchBox->text());
-    });
+    connect(m_searchTimer, &QTimer::timeout, this, [this]() { filterIcons(m_searchBox->text()); });
 
     // 计数标签
     m_countLabel = new QLabel(this);
@@ -76,77 +74,131 @@ void IconGrid::setupUI() {
     m_scrollArea->setWidget(m_scrollContent);
     m_scrollArea->setWidgetResizable(true);
 
+    // 分页控件
+    m_paginationLayout = new QHBoxLayout;
+    m_prevButton       = new QPushButton("上一页", this);
+    m_nextButton       = new QPushButton("下一页", this);
+    m_pageLabel        = new QLabel(this);
+
+    m_paginationLayout->addStretch();
+    m_paginationLayout->addWidget(m_prevButton);
+    m_paginationLayout->addWidget(m_pageLabel);
+    m_paginationLayout->addWidget(m_nextButton);
+    m_paginationLayout->addStretch();
+
+    connect(m_prevButton, &QPushButton::clicked, this, [this]() {
+        if (m_currentPage > 0) {
+            onPageChanged(m_currentPage - 1);
+        }
+    });
+    connect(m_nextButton, &QPushButton::clicked, this, [this]() {
+        if (m_currentPage < m_totalPages - 1) {
+            onPageChanged(m_currentPage + 1);
+        }
+    });
+
     // 添加到主布局
     m_mainLayout->addWidget(m_searchBox);
     m_mainLayout->addWidget(m_countLabel);
     m_mainLayout->addWidget(m_scrollArea);
+    m_mainLayout->addLayout(m_paginationLayout);
 }
 
 void IconGrid::clearIcons() {
-    // 清除现有图标
-    for (auto button : m_iconButtons.values()) {
+    clearCurrentPageButtons();
+    m_filteredIcons.clear();
+    m_originalIcons.clear();
+}
+
+void IconGrid::clearCurrentPageButtons() {
+    // 清除并删除当前页的所有按钮
+    for (auto button : m_currentPageButtons.values()) {
         m_gridLayout->removeWidget(button);
         delete button;
     }
-    m_iconButtons.clear();
+    m_currentPageButtons.clear();
 }
 
 void IconGrid::setIcons(const QMap<int, QString>&                            iconMap,
                         const std::function<QIcon(int, int, const QColor&)>& iconProvider) {
     clearIcons();
-
-    // 保存图标提供者函数
     m_iconProvider = iconProvider;
 
-    // 创建所有图标按钮
+    // 存储原始图标数据
+    m_originalIcons.reserve(iconMap.size());
     for (auto it = iconMap.begin(); it != iconMap.end(); ++it) {
-        int     iconCode = it.key();
-        QString name     = it.value();
-
-        IconButton* button = new IconButton(iconCode, name, this);
-        button->setIcon(m_iconProvider(iconCode, 32, Qt::black));
-        m_iconButtons[name] = button;
+        m_originalIcons.append({it.key(), it.value()});
     }
 
-    // 初始布局所有图标
+    // 初始显示所有图标
     filterIcons("");
-
-    m_countLabel->setText(QString("共 %1 个图标").arg(iconMap.size()));
 }
 
 void IconGrid::filterIcons(const QString& filter) {
-    int visibleCount = 0;
+    clearCurrentPageButtons();
 
-    // 暂时禁用布局更新
-    m_scrollContent->setUpdatesEnabled(false);
+    // 从原始数据中过滤
+    m_filteredIcons.clear();
+    m_filteredIcons.reserve(m_originalIcons.size());
 
-    // 先清除所有部件的父对象关系，但不删除它们
-    for (auto button : m_iconButtons.values()) {
-        m_gridLayout->removeWidget(button);
-    }
-
-    QList<IconButton*> visibleButtons;
-    for (auto it = m_iconButtons.begin(); it != m_iconButtons.end(); ++it) {
-        bool match = filter.isEmpty() || it.key().contains(filter, Qt::CaseInsensitive);
-        if (match) {
-            visibleButtons.append(it.value());
-            visibleCount++;
+    for (const auto& iconData : m_originalIcons) {
+        if (filter.isEmpty() || iconData.second.contains(filter, Qt::CaseInsensitive)) {
+            m_filteredIcons.append(iconData);
         }
-        it.value()->hide();
     }
 
-    // 批量更新布局
-    int maxCols = 8;
-    for (int i = 0; i < visibleButtons.size(); ++i) {
-        int row = i / maxCols;
-        int col = i % maxCols;
-        m_gridLayout->addWidget(visibleButtons[i], row, col);
-        visibleButtons[i]->show();
+    // 计算总页数
+    m_totalPages  = (m_filteredIcons.size() + ICONS_PER_PAGE - 1) / ICONS_PER_PAGE;
+    m_currentPage = 0;
+
+    // 更新分页控件状态
+    m_prevButton->setEnabled(false);
+    m_nextButton->setEnabled(m_totalPages > 1);
+
+    // 显示当前页
+    onPageChanged(0);
+
+    m_countLabel->setText(QString("共 %1 个图标").arg(m_filteredIcons.size()));
+}
+
+void IconGrid::onPageChanged(int page) {
+    clearCurrentPageButtons();
+
+    m_currentPage = page;
+
+    // 计算当前页的图标范围
+    int startIndex = page * ICONS_PER_PAGE;
+    int endIndex   = qMin(startIndex + ICONS_PER_PAGE, m_filteredIcons.size());
+
+    // 创建并显示当前页的图标
+    int maxCols    = 8;
+    int currentRow = 0;
+    int currentCol = 0;
+
+    // 重置布局
+    QLayoutItem* item;
+    while ((item = m_gridLayout->takeAt(0)) != nullptr) {
+        delete item;
     }
 
-    m_countLabel->setText(QString("显示 %1/%2 个图标").arg(visibleCount).arg(m_iconButtons.size()));
+    for (int i = startIndex; i < endIndex; ++i) {
+        const auto& iconData = m_filteredIcons[i];
+        IconButton* button   = new IconButton(iconData.first, iconData.second, m_scrollContent);
+        button->setIcon(m_iconProvider(iconData.first, 32, Qt::black));
 
-    // 重新启用更新并强制刷新
-    m_scrollContent->setUpdatesEnabled(true);
-    m_scrollContent->update();
+        m_gridLayout->addWidget(button, currentRow, currentCol);
+        m_currentPageButtons[i - startIndex] = button;
+
+        // 更新行列位置
+        currentCol++;
+        if (currentCol >= maxCols) {
+            currentCol = 0;
+            currentRow++;
+        }
+    }
+
+    // 更新分页控件
+    m_prevButton->setEnabled(page > 0);
+    m_nextButton->setEnabled(page < m_totalPages - 1);
+    m_pageLabel->setText(QString("第 %1/%2 页").arg(page + 1).arg(m_totalPages));
 }
--- examples/icongrid.h
+++ examples/icongrid.h
@@ -1,58 +1,72 @@
 #ifndef ICONGRID_H
 #define ICONGRID_H
 
-#include <QWidget>
 #include <QGridLayout>
-#include <QPushButton>
-#include <QScrollArea>
+#include <QLabel>
 #include <QLineEdit>
 #include <QMap>
-#include <QLabel>
+#include <QPushButton>
+#include <QScrollArea>
+#include <QWidget>
 #include <functional>
 
 class IconButton : public QPushButton {
     Q_OBJECT
-public:
+  public:
     explicit IconButton(int iconCode, const QString& name, QWidget* parent = nullptr);
     int iconCode() const { return m_iconCode; }
-    
-private slots:
+
+  private slots:
     void copyIconName();
-    
-private:
+
+  private:
     QString m_iconName;
-    int m_iconCode;
+    int     m_iconCode;
 };
 
 class IconGrid : public QWidget {
     Q_OBJECT
-public:
+  public:
     explicit IconGrid(QWidget* parent = nullptr);
-    
+
     // 设置图标集合
     void setIcons(const QMap<int, QString>& iconMap, const std::function<QIcon(int, int, const QColor&)>& iconProvider);
 
     void clearIcons();
 
-    signals:
+  signals:
     void iconClicked(int iconCode, const QString& iconName);
-    
-private slots:
-    void filterIcons(const QString& filter);
-    
-protected:
-    void setupUI();
 
-    QVBoxLayout* m_mainLayout;
-    QGridLayout* m_gridLayout;
-    QScrollArea* m_scrollArea;
-    QWidget* m_scrollContent;
-    QLineEdit* m_searchBox;
-    QLabel* m_countLabel;
-    QMap<QString, IconButton*> m_iconButtons;
+  private slots:
+    void filterIcons(const QString& filter);
+    void onPageChanged(int page);
+
+  private:
+    static const int ICONS_PER_PAGE = 104;
+
+    void setupUI();
+    void clearCurrentPageButtons();
+
+    QVBoxLayout*                                  m_mainLayout;
+    QGridLayout*                                  m_gridLayout;
+    QScrollArea*                                  m_scrollArea;
+    QWidget*                                      m_scrollContent;
+    QLineEdit*                                    m_searchBox;
+    QLabel*                                       m_countLabel;
+    QMap<int, IconButton*>                        m_currentPageButtons;
+    QVector<QPair<int, QString>>                  m_originalIcons;
     std::function<QIcon(int, int, const QColor&)> m_iconProvider;
 
     QTimer* m_searchTimer;
+
+    // 分页相关
+    QHBoxLayout*                 m_paginationLayout;
+    QPushButton*                 m_prevButton;
+    QPushButton*                 m_nextButton;
+    QLabel*                      m_pageLabel;
+    QVector<QPair<int, QString>> m_filteredIcons; // 存储过滤后的图标数据
+    int                          m_currentPage;
+    int                          m_totalPages;
 };
 
-#endif // ICONGRID_H 
+#endif // ICONGRID_H
